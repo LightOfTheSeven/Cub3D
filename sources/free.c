0f6e4c27f9ca35f15d31d9ebba58b6031769938b
Gael BERTIN <gbertin@e2r1p9.clusters.42paris.fr>
2022-12-02 08:45:32 +0100

    fix leak img

--- sources/free.c
+++ sources/free.c
@@ -6,7 +6,7 @@
 /*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 09:19:55 by gbertin           #+#    #+#             */
-/*   Updated: 2022/11/29 12:37:01 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/12/02 08:45:01 by gbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,8 @@ int	free_general(t_general *general)
 {
 	if (general->map->matrice)
 		free_tab(general->map->matrice);
+	if (general->mlx.img)
+		mlx_destroy_image(general->mlx.ptr, general->mlx.img);
 	free_spts(general);
 	free(general->map);
 	return (0);
